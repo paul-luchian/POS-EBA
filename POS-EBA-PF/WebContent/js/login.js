87efe84007d1da9ae1e81a631a77a0aa0d2ccb1e
adelina <adelina@adelina-VirtualBox>
2019-04-10 01:58:59 +0300

    Login service

--- POS-EBA-PF/WebContent/js/login.js
+++ POS-EBA-PF/WebContent/js/login.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
-	$("button#login").on("click", function()
+	$("button#login").on("click", function(e)
 	{
+		e.preventDefault();
 		var email=$('input[name="email"]').val();
-		var password = $('input[name="password"]').var();
-		var arg = new Object();
-		arg.name=email;
-		arg.value=password;
-		console.log(arg);
+		var password = $('input[name="password"]').val();
+		var arg = {
+				"email":email,
+				"password":password
+		}
+		var json = JSON.stringify(arg);
+		console.log("login_json="+json);
+		jQuery.support.cors = true;
+		$.ajax({
+			type:'POST', 
+			url:"http://localhost:8180/POS-EBA-RS/services/validation/login",
+			crossDomain: true,
+			data:json,
+			contentType:"application/json; charset=utf-8",
+			dataType:"json",
+			
+			success:function(data)
+			{
+					console.log( "response post"+ "error"+JSON.stringify(data));
+					window.location.replace("http://localhost:8180/POS-EBA-PF/profile.jsp");
+
+			},
+			error:function(data)
+			{
+				console.log("error"+JSON.stringify(data));
+			}
+		});
 		//location.assign("localhost:8080/POS-EBA-RS/services/validator/profile");
 	})
 
